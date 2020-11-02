63a239961263022b80d4b5fa3f67312e44c3a798
mio <mio@inno-orz.com>
2020-11-02 15:14:14 +0800

    fix.

--- MBaseSDK/build/MBaseSDK.build/Debug-iphoneos/MBaseSDK.build/DerivedSources/MBaseSDK_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char MBaseSDKVersionString[];
- extern const double MBaseSDKVersionNumber;
-
- const unsigned char MBaseSDKVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MBaseSDK  PROJECT:MBaseSDK-26" "\n";
- const double MBaseSDKVersionNumber __attribute__ ((used)) = (double)26.;
